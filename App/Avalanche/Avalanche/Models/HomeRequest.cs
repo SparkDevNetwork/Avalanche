1bc786a02076d87f29cbc65fb8a6d4851834051f
Mark Lee <mlee@secc.org>
2018-12-14 10:32:13 -0500

    Fixed merge and formatting

--- App/Avalanche/Avalanche/Models/HomeRequest.cs
+++ App/Avalanche/Avalanche/Models/HomeRequest.cs
@@ -1,6 +1,18 @@
-﻿using System;
+﻿// <copyright>
+// Copyright Southeast Christian Church
+//
+// Licensed under the  Southeast Christian Church License (the "License");
+// you may not use this file except in compliance with the License.
+// A copy of the License shoud be included with this file.
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+// </copyright>
+//
 using System.Collections.Generic;
-using System.Text;
 
 namespace Avalanche.Models
 {
@@ -9,5 +21,6 @@ namespace Avalanche.Models
         public MobilePage Header { get; set; }
         public MobilePage Footer { get; set; }
         public MobilePage Page { get; set; }
+        public IDictionary<string, string> Attributes { get; set; }
     }
 }
