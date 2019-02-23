93d23640b7783f044c589ca6be71b32f96856f50
Isaiah Williams <idwilliams@outlook.com>
2019-02-23 00:52:31 -0600

    Converted the Xam.Forms.MarkdownView project to .NET Standard

--- App/Avalanche/Avalanche.Android/Resources/Resource.Designer.cs
+++ App/Avalanche/Avalanche.Android/Resources/Resource.Designer.cs
@@ -5996,26 +5996,26 @@ namespace Avalanche.Droid
 			// aapt resource value: 0x7f020054
 			public const int avd_hide_password = 2130837588;
 			
-			// aapt resource value: 0x7f02012e
-			public const int avd_hide_password_1 = 2130837806;
-			
 			// aapt resource value: 0x7f02012f
-			public const int avd_hide_password_2 = 2130837807;
+			public const int avd_hide_password_1 = 2130837807;
 			
 			// aapt resource value: 0x7f020130
-			public const int avd_hide_password_3 = 2130837808;
+			public const int avd_hide_password_2 = 2130837808;
+			
+			// aapt resource value: 0x7f020131
+			public const int avd_hide_password_3 = 2130837809;
 			
 			// aapt resource value: 0x7f020055
 			public const int avd_show_password = 2130837589;
 			
-			// aapt resource value: 0x7f020131
-			public const int avd_show_password_1 = 2130837809;
-			
 			// aapt resource value: 0x7f020132
-			public const int avd_show_password_2 = 2130837810;
+			public const int avd_show_password_1 = 2130837810;
 			
 			// aapt resource value: 0x7f020133
-			public const int avd_show_password_3 = 2130837811;
+			public const int avd_show_password_2 = 2130837811;
+			
+			// aapt resource value: 0x7f020134
+			public const int avd_show_password_3 = 2130837812;
 			
 			// aapt resource value: 0x7f020056
 			public const int common_full_open_on_phone = 2130837590;
@@ -6629,11 +6629,11 @@ namespace Avalanche.Droid
 			// aapt resource value: 0x7f020121
 			public const int notification_icon_background = 2130837793;
 			
-			// aapt resource value: 0x7f02012c
-			public const int notification_template_icon_bg = 2130837804;
-			
 			// aapt resource value: 0x7f02012d
-			public const int notification_template_icon_low_bg = 2130837805;
+			public const int notification_template_icon_bg = 2130837805;
+			
+			// aapt resource value: 0x7f02012e
+			public const int notification_template_icon_low_bg = 2130837806;
 			
 			// aapt resource value: 0x7f020122
 			public const int notification_tile_bg = 2130837794;
@@ -6642,28 +6642,31 @@ namespace Avalanche.Droid
 			public const int notify_panel_notification_icon_bg = 2130837795;
 			
 			// aapt resource value: 0x7f020124
-			public const int portrait_mode = 2130837796;
+			public const int pixel = 2130837796;
 			
 			// aapt resource value: 0x7f020125
-			public const int profilePlaceholder = 2130837797;
+			public const int portrait_mode = 2130837797;
 			
 			// aapt resource value: 0x7f020126
-			public const int sermonplaceholder = 2130837798;
+			public const int profilePlaceholder = 2130837798;
 			
 			// aapt resource value: 0x7f020127
-			public const int Splash1 = 2130837799;
+			public const int sermonplaceholder = 2130837799;
 			
 			// aapt resource value: 0x7f020128
-			public const int Splash2 = 2130837800;
+			public const int Splash1 = 2130837800;
 			
 			// aapt resource value: 0x7f020129
-			public const int Splash3 = 2130837801;
+			public const int Splash2 = 2130837801;
 			
 			// aapt resource value: 0x7f02012a
-			public const int splash_centered = 2130837802;
+			public const int Splash3 = 2130837802;
 			
 			// aapt resource value: 0x7f02012b
-			public const int xam_mediamanager_notify_ic = 2130837803;
+			public const int splash_centered = 2130837803;
+			
+			// aapt resource value: 0x7f02012c
+			public const int xam_mediamanager_notify_ic = 2130837804;
 			
 			static Drawable()
 			{
